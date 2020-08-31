357d54b89ddcccc2486ecf375fd49732775e31d9
creio <mailcreio@gmail.com>
2020-08-31 11:50:25 +0300

    31.08.2020 Update

--- etc/skel/.config/bspwm/multilock.sh
+++ etc/skel/.config/bspwm/multilock.sh
@@ -2,7 +2,7 @@
 
 IMG=/usr/share/wall/wl.png
 
-if [[ `which multilockscreen` != 'multilockscreen not found' ]]; then
+if [[ "$(which multilockscreen)" != "multilockscreen not found" ]]; then
   if [[ ! -d $HOME/.cache/multilock ]]; then
     multilockscreen -u $IMG --blur 0.5
   fi
